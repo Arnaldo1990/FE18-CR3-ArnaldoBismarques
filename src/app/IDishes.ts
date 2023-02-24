3d39626ee09132fe4a6caf922532b3752165a908
Arnaldo Bismarques <arnaldo.bismarques@hotmail.com>
2023-02-24 16:44:50 +0100

    changes menu

---
+++ src/app/IDishes.ts
@@ -0,0 +1,7 @@
+export interface IDishes{
+    pic: string; 
+    name: string; 
+    price: number; 
+    description: string;
+    availability?: string;
+    }
---
+++ src/app/dishes.ts
@@ -0,0 +1,78 @@
+export const dishes = [{
+
+pic: "https://media.istockphoto.com/id/1040425298/de/foto/pepperoni-pizza.jpg?b=1&s=170667a&w=0&k=20&c=prRHFCX-isvl_Bupq7lWI1usFJG0s4zsL2NiI0VQVeA=",
+
+name: "Pizza Al Capone",
+
+price: 8.50,
+
+description: "with mozzarella, tomatoe sauce, spicy salami and mushrooms",
+
+availability: "Yes",
+
+} ,{
+
+    pic: "https://media.istockphoto.com/id/638778818/it/foto/pizza-tradizionale-italiana-con-salsa-di-pomodoro-aglio-basilico-e-origano.jpg?b=1&s=170667a&w=0&k=20&c=V276rCny8LmfkT1mMi-73oseEtorC95GsY6qmllZ2XI=",
+
+    name: "Pizza Marinara",
+    
+    price: 6.00,
+    
+    description: "with mozzarrella, tomatoesauce and garlic",
+
+    availability: "Yes",
+    
+}, {
+    
+    pic: "https://cdn.pixabay.com/photo/2016/08/28/15/31/pizza-1626188__340.jpg",
+
+name: "Pizza Al Tonno",
+
+price: 9.00,
+
+description: "with mozzarrella, tomatoesauce, black olives, tuna and onions",
+
+availability: "Yes",
+
+},{
+    pic: "https://cdn.pixabay.com/photo/2015/05/27/18/53/spaghetti-787048__340.jpg",
+
+name: "Spaghetti Bolognese",
+
+price: 7.00,
+
+description: "our housemade noodles, served with a tomatoe sauce and fresh meatballs",
+
+},{
+    pic: "https://cdn.pixabay.com/photo/2017/02/15/15/17/meal-2069021__340.jpg",
+
+    name: "Lasagne Al Forno",
+    
+    price: 8.00,
+    
+    description: "our housemade lasagne, served with meat (beef and pork), tomatoesauce, parmesano and carrots",
+
+    availability: "Yes",
+
+},{
+    pic: "https://cdn.pixabay.com/photo/2018/02/12/07/54/calamari-3147644__340.jpg",
+
+    name: "Calamari Fritti",
+    
+    price: 15.50,
+    
+    description: "fried octopus served with potatoe fries and nana's homemade cocktail sauce",
+
+    availability: "Yes",
+
+},{
+    pic: "https://cdn.pixabay.com/photo/2017/03/19/18/22/italian-food-2157246__340.jpg",
+
+    name: "Tiramisu",
+    
+    price: 5.50,
+    
+    description: "our sweet homemade dessert with local ingredients",
+
+}]
+
